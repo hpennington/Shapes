acdf12286e84ba12c22492c5d1633d0b149e4c54
Hayden Pennington <haydenpennington@icloud.com>
2021-11-29 11:13:56 -0600

    Remove frame

--- ShapesSwiftBook/CircleDoc.swift
+++ ShapesSwiftBook/CircleDoc.swift
@@ -20,7 +20,7 @@ struct CircleBlue: View {
                     .foregroundColor(.red)
                     .scaleEffect(3.0)
                     
-            }.frame(maxWidth: 1000, alignment: .leading)
+            }
             P("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
             P("It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.")
             SwiftBookComponent {
