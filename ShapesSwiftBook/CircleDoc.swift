de0396f4001d064cd89a6b6ab35ef6a16eda8941
Hayden Pennington <haydenpennington@icloud.com>
2021-11-29 07:08:54 -0600

    Add vstacks to doc

--- ShapesSwiftBook/CircleDoc.swift
+++ ShapesSwiftBook/CircleDoc.swift
@@ -12,11 +12,13 @@ struct CircleBlue: View {
     @Binding var color: Color
     
     var body: some View {
-        H1("A blue circle")
-        P("It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.")
-        P("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
-        CircleView(color: color)
-            .padding()
+        VStack {
+            H1("A blue circle")
+            P("It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.")
+            P("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
+            CircleView(color: color)
+                .padding()
+        }
     }
 }
 
@@ -24,10 +26,12 @@ struct CircleRed: View {
     @Binding var color: Color
     
     var body: some View {
-        H1("A red circle")
-        H3("This is a description of a red circle.")
-        CircleView(color: color)
-            .padding()
+        VStack {
+            H1("A red circle")
+            H3("This is a description of a red circle.")
+            CircleView(color: color)
+                .padding()
+        }
     }
 }
 
